ce48550018b4fbb1a43e6743cb8268ae0d7d5366
oliv <cheroliv92@gmail.com>
2018-10-17 18:34:01 +0200

    _

--- 01-angular-hello-world/src/app/user-item/user-item.component.ts
+++ 01-angular-hello-world/src/app/user-item/user-item.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-item.component.css']
 })
 export class UserItemComponent implements OnInit {
+  name: string; // <-- added name property
 
-  constructor() { }
+  constructor() {
+    this.name = 'Felipe'; // set the name
+   }
 
   ngOnInit() {
   }
-
 }
