ebf8f9c648f8ce433d08a0164353d98ef90ea67e
oliv <cheroliv92@gmail.com>
2018-10-18 20:24:06 +0200

    _

--- 01-angular-hello-world/src/app/app.module.ts
+++ 01-angular-hello-world/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
-import { UserItemComponent } from './user-item/user-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent,
-    UserItemComponent
+    HelloWorldComponent
   ],
   imports: [
     BrowserModule
--- 03-angular-hello-world-user-list/src/app/app.module.ts
+++ 03-angular-hello-world-user-list/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserItemComponent } from './user-item/user-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule
